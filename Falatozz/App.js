47c6f81bf37b56a93411541353e71dd32ff360aa
Attila Galambos <49750281+galambati@users.noreply.github.com>
2022-07-07 11:26:25 +0200

    initialize menu.css, implement 2 basic buttons

--- Falatozz/App.js
+++ Falatozz/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Menu from "./menu";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Falatozz.hu!</Text>
       <StatusBar style="auto" />
+      <Menu/>
     </View>
   );
 }
---
+++ Falatozz/menu.js
@@ -0,0 +1,13 @@
+import {Button, View} from "react-native";
+
+
+function Menu() {
+    return(
+        <View>
+            <Button title="Read"></Button>
+            <Button title="Create"></Button>
+        </View>
+    )
+}
+
+export default Menu;
